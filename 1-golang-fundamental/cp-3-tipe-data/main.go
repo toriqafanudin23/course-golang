800a45ae216a415089d6525015f5469f6462fbc6
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-05-17 06:00:49 +0700

    cp-3

---
+++ 1-golang-fundamental/cp-3-tipe-data/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var positiveNumber uint8 = 90
+	var negatifveNumber int = -90
+	var decimalNumber float32 = 3.9345
+
+	fmt.Println("Positif with uint8 :", positiveNumber)
+	fmt.Println("Negatif with int :", negatifveNumber)
+	fmt.Println("Decimal with float32 :", decimalNumber)
+
+	fmt.Printf("Dua angka dibelakang koma : %.2f \n", decimalNumber)
+
+	var exist = true
+	fmt.Printf("exist? %t \n", exist)
+
+	var message = "Halooo"
+	fmt.Printf("Faisa, %s", message)
+}

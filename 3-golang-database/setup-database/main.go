f294966a496c7d69ed6bb4ae2b224574b805845b
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-06-02 08:36:01 +0700

    menyembunyikan password

--- 3-golang-database/setup-database/main.go
+++ 3-golang-database/setup-database/main.go
@@ -16,28 +16,18 @@ import (
 
 var psqlInfo string
 
-// const (
-// 	host     = "localhost"
-// 	port     = 5432
-// 	user     = "postgres"
-// 	password = "F@1sanirbita"
-// 	dbname   = "enigmacamp"
-// )
-
 func init() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	// Pindahkan ke sini setelah .env diload
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	// Gunakan %s untuk port karena port dari .env adalah string
 	psqlInfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 }
 

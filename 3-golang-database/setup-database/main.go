52c9395f99e4fc9e809e7ce4e5ae9c79eb4277b5
toriqafanudin23 <toriqafanudin23@gmail.com>
2025-06-02 08:07:43 +0700

    golang database DML DQL

---
+++ 3-golang-database/setup-database/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"setup-database/entity"
+
+	// "time"
+
+	// "github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
+)
+
+// const (
+// 	host     = "localhost"
+// 	port     = 5432
+// 	user     = "postgres"
+// 	password = "F@1sanirbita"
+// 	dbname   = "enigmacamp"
+// )
+
+const (
+	host     = "aws-0-ap-southeast-1.pooler.supabase.com"
+	port     = 6543
+	user     = "postgres.abwzragmndkshpeiivjl"
+	password = "F@1sanirbita"
+	dbname   = "postgres"
+)
+
+var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+func main() {
+	// student := entity.Student{Id: 5, Name: "Dhiya Rohadatul Aisy", Email: "aisy@gmail.com", Address: "Wonosobo", BirthDate: time.Date(1999, 06, 16, 0, 0, 0, 0, time.Local), Gender: "F"}
+
+	// addStudent(student)
+	// updateStudent(student)
+	// deleteStudent("4")
+	students := getAllStudent()
+	// for _, student := range students {
+	// 	fmt.Println(student.Id, student.Name, student.Email, student.Address, student.BirthDate, student.Gender)
+	// }
+	// fmt.Println(getStudentById(2))
+	fmt.Println(students)
+}
+
+func addStudent(student entity.Student) {
+	db := connectDb()
+	defer db.Close()
+	var err error
+
+	sqlStatement := "INSERT INTO mst_student (id, name, email, address, birth_date, gender) VALUES ($1, $2, $3, $4, $5, $6);"
+
+	_, err = db.Exec(sqlStatement, student.Id, student.Name, student.Email, student.Address, student.BirthDate, student.Gender)
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Succesfully Insert Data!")
+	}
+}
+
+func updateStudent(student entity.Student) {
+	db := connectDb()
+	defer db.Close()
+	var err error
+
+	sqlStatement := "UPDATE mst_student SET name = $2, email = $3, address = $4, birth_date = $5, gender = $6 WHERE id = $1;"
+
+	_, err = db.Exec(sqlStatement, student.Id, student.Name, student.Email, student.Address, student.BirthDate, student.Gender)
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Succesfully Update Data!")
+	}
+}
+
+func deleteStudent(id string) {
+	db := connectDb()
+	defer db.Close()
+	var err error
+
+	sqlStatement := "DELETE FROM mst_student WHERE id = $1;"
+
+	_, err = db.Exec(sqlStatement, id)
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Succesfully Delete Data!")
+	}
+}
+
+func getAllStudent() []entity.Student {
+	db := connectDb()
+	defer db.Close()
+
+	sqlStatement := "SELECT * FROM mst_student;"
+	rows, err := db.Query(sqlStatement)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	students := scanStudent(rows)
+
+	return students
+}
+
+func scanStudent(rows *sql.Rows) []entity.Student {
+	students := []entity.Student{}
+	var err error
+
+	for rows.Next() {
+		student := entity.Student{}
+		err := rows.Scan(&student.Id, &student.Name, &student.Email, &student.Address, &student.BirthDate, &student.Gender)
+		if err != nil {
+			panic(err)
+		}
+
+		students = append(students, student)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	return students
+}
+
+func getStudentById(id int) entity.Student {
+	db := connectDb()
+	defer db.Close()
+	var err error
+
+	sqlStatement := "SELECT * FROM mst_student WHERE id = $1"
+
+	student := entity.Student{}
+	err = db.QueryRow(sqlStatement, id).Scan(&student.Id, &student.Name, &student.Email, &student.Address, &student.BirthDate, &student.Gender)
+	if err != nil {
+		panic(err)
+	}
+
+	return student
+}
+
+func connectDb() *sql.DB {
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Succesfully connected!")
+	}
+	return db
+}
